4bc73425d5320f635451fefa095670eba08d2c91
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-05-22 16:48:43 -0700

    Added function for loading trip details.

--- noise.js
+++ noise.js
@@ -1,3 +1,24 @@
+const play = function play(audio) {
+  audio.pause();
+  audio.currentTime = 0;
+  audio.play();
+}
+
 $(document).ready( function() {
-  // your code here
+
+  $('.instrument').on('click', 'button', function() {
+    let audioID = this.innerHTML + 'Audio';
+    let audioFile = $(`#${audioID}`)[0];
+    play(audioFile);
+  });
+
+  $('body').keydown((event) => {
+    const notes = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
+
+    if (notes.indexOf(event.key) > -1) {
+      let audioID = event.key + 'Audio';
+      let audioFile = $(`#${audioID}`)[0];
+      play(audioFile);
+    }
+  });
 });
